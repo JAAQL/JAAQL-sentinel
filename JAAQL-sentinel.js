c6be1872ffdc98c867ae2a3168479c8fb5cbdc1b
Aaron Tasker <45070792+aarontasker@users.noreply.github.com>
2022-07-05 23:18:48 +0200

    Version 1.0.1

--- JAAQL-sentinel.js
+++ JAAQL-sentinel.js
@@ -5,6 +5,7 @@ if (!window.JAAQL_SENTINEL_URL.startsWith("http")) {
         window.JAAQL_SENTINEL_URL += "/api";
     }
 }
+window.JAAQL_SENTINEL_URL += "/sentinel/reporting/error";
 
 window.JAAQL_SENTINEL_APP_VERSION = "no-version";
 if (document.currentScript.hasAttribute("app_version")) {
@@ -15,10 +16,10 @@ window.JAAQL_SENTINEL_APP_NAME = document.currentScript.getAttribute("app_name")
 
 window.addEventListener("error", function(evt) {
     let xhttp = new XMLHttpRequest();
-    xhttp.open("POST", window.JAAQL_SENTINEL_URL + "/", true);
+    xhttp.open("POST", window.JAAQL_SENTINEL_URL, true);
 
     let stack = null;
-    if (evt.error.hasAttribute("stack")) {
+    if (evt.error.stack) {
         stack = evt.error.stack;
     }
 
@@ -33,5 +34,5 @@ window.addEventListener("error", function(evt) {
     };
 
     xhttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-    xhttp.send(JSON.stringify(json));
+    xhttp.send(JSON.stringify(errorData));
 });
